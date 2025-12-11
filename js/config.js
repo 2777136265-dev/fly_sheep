05eee69db8c346c5c941bd68ff8ecc424d186c5b
2777136265-dev <2777136265@qq.com>
2025-12-11 13:10:15 +0800

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,12 @@
+// config.js
+export const SUPABASE_CONFIG = {
+  url: 'https://neflfdfpzyjookonmleo.supabase.co',
+  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
+};
+
+export const MAIL_CONFIG = {
+  apiUrl: '/api/wx_mail/send',
+  token: 'oqrUZ6_DEc0gc4YBGvRlygSCiHY4'
+};
+
+export const ADMIN_EMAIL = 'admin@example.com';
